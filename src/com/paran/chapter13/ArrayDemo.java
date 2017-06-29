a4501a3d07c7959ffdbbdf2a87d81d4dcd9a0343
taroakrkr <taroakrkr@gmail.com>
2017-06-29 14:33:53 +0900

    버블소트

--- src/com/paran/chapter13/ArrayDemo.java
+++ src/com/paran/chapter13/ArrayDemo.java
@@ -6,20 +6,28 @@ public class ArrayDemo{
 		int[] intArray = {1,2,3,4,5,6,7,8,9,10};
 		
 		System.out.println("인트배열");
-		for(int obj : intArray){
-			System.out.println(obj);
-		}
+		showArray(intArray);
 		
 		System.out.println("리버스함수 호출 후");
 		reverseArrayValue(intArray);
 		
-		for(int obj : intArray){
+		showArray(intArray);
+		
+		System.out.println("버블소트");
+		int[] sortArr = {6,8,4,2,5,1,3};
+		bubbleSort(sortArr, true);
+		showArray(sortArr);
+		
+	}
+
+	static void showArray(int[] array){
+		for(int obj : array){
 			System.out.println(obj);
 		}
 	}
-
+	
 	static void reverseArrayValue(int[] array){
-		int[] temp= new int[array.length];
+		int[] temp = new int[array.length];
 
 		for(int i = 0; i < array.length; i++){
 			temp[array.length-(i+1)]=array[i];
@@ -28,5 +36,30 @@ public class ArrayDemo{
 			array[i]=temp[i];
 		}
 
+	}
+	
+	static void bubbleSort(int[] array, boolean asc){
+		int temp = 0;
+		if(asc){
+			for(int i = 0; i < array.length - 1; i++){
+				for(int j = 0; j<array.length-1-i; j++){
+					if(array[j]>array[j+1]){
+						temp=array[j];
+						array[j]=array[j+1];
+						array[j+1]=temp;
+					}
+				}
+			}
+		}else{
+			for(int i = 0; i < array.length - 1; i++){
+				for(int j = 0; j<array.length-1-i; j++){
+					if(array[j]<array[j+1]){
+						temp=array[j];
+						array[j]=array[j+1];
+						array[j+1]=temp;
+					}
+				}
+			}
+		}
 	}
 }
