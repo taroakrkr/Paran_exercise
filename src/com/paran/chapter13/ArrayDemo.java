8a8b25a8348c60d585e8b1cb27477046230e8ebd
taroakrkr <taroakrkr@gmail.com>
2017-06-29 13:08:53 +0900

    리버스배열

--- src/com/paran/chapter13/ArrayDemo.java
+++ src/com/paran/chapter13/ArrayDemo.java
@@ -7,10 +7,12 @@ public class ArrayDemo{
 	public static void main(String[] args){
 		int[] intArray = {1,2,3,4,5,6,7,8,9,10};
 		
+		System.out.println("인트배열");
 		for(int obj : intArray){
 			System.out.println(obj);
 		}
 		
+		System.out.println("리버스함수 호출 후");
 		reverseArrayValue(intArray);
 		
 		for(int obj : intArray){
@@ -19,12 +21,14 @@ public class ArrayDemo{
 	}
 
 	static void reverseArrayValue(int[] array){
-		int[] temp = {1,2,3,4,5,6,7,8,9,10};
+		int[] temp= new int[array.length];
+
 		for(int i = 0; i < array.length; i++){
 			temp[array.length-(i+1)]=array[i];
 		}
 		for(int i = 0; i < array.length; i++){
 			array[i]=temp[i];
 		}
+
 	}
 }
