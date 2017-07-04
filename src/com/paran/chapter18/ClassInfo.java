92fc57ad58ad94b110d4a0a9680eb9f23910ed93
taroakrkr <taroakrkr@gmail.com>
2017-07-04 14:03:03 +0900

    리스트를 클래스로 따로 뺌

--- src/com/paran/chapter18/ClassInfo.java
+++ src/com/paran/chapter18/ClassInfo.java
@@ -10,78 +10,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClassInfo {
-	public static void initStudent(List<Student> stuList){
-		// 파일 변수 file을 만든다
-		File file = new File("C:\\student.txt");
-    	try {
-    		// BufferedReader 변수에 file을 넣는다
-    		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file),"euc-kr"));
-
-
-        	// 파일을 한줄씩 읽어 넣기 위한 변수 line
-        	String line = null;
-
-        	// 하나의 line을 split 하여 넣을 배열 splitedStr
-        	String[] splitedStr = null;
-        	int score=0;
-
-        	// 한 줄씩 읽어서 line에 넣은 후 null이 아니면 실행
-        	while( (line = reader.readLine()) != null ) {
-
-            	// 초기화
-            	splitedStr = null;
-
-            	// 탭을 기준으로 잘라서 splitedStr 에 넣는다
-            	splitedStr = line.split("\t");
-
-            	// 배열에 들어간 길이 만큼 반복한다
-            	for (int i = 0; i < splitedStr.length; i++) {
-            		if(i==3){
-            			score = Integer.valueOf(splitedStr[3]);//점수는 int로
-            		}else{
-            			// 양쪽의 공백을 제거하고 다시 입력한다
-            			splitedStr[i] = splitedStr[i].trim();
-            		}
-                    // splitedStr 을 List<Class명>에 입력하는 등 이용가능하다
-            	}
-            	if(splitedStr[2].substring(0,1).equals("1")){
-            		stuList.add(new DomeStudent(splitedStr[0],splitedStr[1],splitedStr[2],score));
-            	}else
-            		stuList.add(new ForeStudent(splitedStr[0],splitedStr[1],splitedStr[2],score));
-        	}
-        	reader.close();
-
-    	} catch (FileNotFoundException fnf) {
-    		fnf.printStackTrace();
-    	} catch( IOException e) {
-        	e.printStackTrace();
-    	}
-	}
-	public static void setRanking(List<Student> stuList){
-		for(int i=0; i<stuList.size(); i++){
-            stuList.get(i).setRank(1); //모두1등으로 초기화
-           
-            for (int j = 0; j < stuList.size(); j++) { //기준데이터와 나머지데이터비교                             
-                if(stuList.get(i).getScore()<stuList.get(j).getScore()){   //기준데이터가 나머지데이터라 비교했을때 적으면 카운트
-                    stuList.get(i).setRank(stuList.get(i).getRank()+1); //COUNT                 
-                }              
-            }          
-        } 
-	}
-	public static void showList(List<Student> stuList){
-		for(int i=0; i<stuList.size(); i++){
-			stuList.get(i).showInfo();
-		}
-	}
-	
 	public static void main(String[] args){
-		List<Student> stuList = new ArrayList<Student>();
-		initStudent(stuList);
-		setRanking(stuList);
-		showList(stuList);
+		StuList obj = new StuList();
+		obj.initStudent();
+		obj.setRanking();
+		obj.showList();
 	}
-	
-	
 }
 
 
@@ -143,5 +77,72 @@ class ForeStudent extends Student{//국외학생
 	}
 	public void showInfo(){
 		System.out.println("외국인 name => "+ getName() + " stuId => "+getStuId()+" resiId => "+foreignId+" score = "+super.getScore()+" ranking = "+ super.getRank());
+	}
+}
+
+class StuList{
+	static List<Student> stuList = new ArrayList<Student>();
+	public void initStudent(){
+		// 파일 변수 file을 만든다
+		File file = new File("C:\\student.txt");
+    	try {
+    		// BufferedReader 변수에 file을 넣는다
+    		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file),"euc-kr"));
+
+
+        	// 파일을 한줄씩 읽어 넣기 위한 변수 line
+        	String line = null;
+
+        	// 하나의 line을 split 하여 넣을 배열 splitedStr
+        	String[] splitedStr = null;
+        	int score=0;
+
+        	// 한 줄씩 읽어서 line에 넣은 후 null이 아니면 실행
+        	while( (line = reader.readLine()) != null ) {
+
+            	// 초기화
+            	splitedStr = null;
+
+            	// 탭을 기준으로 잘라서 splitedStr 에 넣는다
+            	splitedStr = line.split("\t");
+
+            	// 배열에 들어간 길이 만큼 반복한다
+            	for (int i = 0; i < splitedStr.length; i++) {
+            		if(i==3){
+            			score = Integer.valueOf(splitedStr[3]);//점수는 int로
+            		}else{
+            			// 양쪽의 공백을 제거하고 다시 입력한다
+            			splitedStr[i] = splitedStr[i].trim();
+            		}
+                    // splitedStr 을 List<Class명>에 입력하는 등 이용가능하다
+            	}
+            	if(splitedStr[2].substring(0,1).equals("1")){
+            		stuList.add(new DomeStudent(splitedStr[0],splitedStr[1],splitedStr[2],score));
+            	}else
+            		stuList.add(new ForeStudent(splitedStr[0],splitedStr[1],splitedStr[2],score));
+        	}
+        	reader.close();
+
+    	} catch (FileNotFoundException fnf) {
+    		fnf.printStackTrace();
+    	} catch( IOException e) {
+        	e.printStackTrace();
+    	}
+	}
+	public void setRanking(){
+		for(int i=0; i<stuList.size(); i++){
+            stuList.get(i).setRank(1); //모두1등으로 초기화
+           
+            for (int j = 0; j < stuList.size(); j++) { //기준데이터와 나머지데이터비교                             
+                if(stuList.get(i).getScore()<stuList.get(j).getScore()){   //기준데이터가 나머지데이터라 비교했을때 적으면 카운트
+                    stuList.get(i).setRank(stuList.get(i).getRank()+1); //COUNT                 
+                }              
+            }          
+        } 
+	}
+	public void showList(){
+		for(int i=0; i<stuList.size(); i++){
+			stuList.get(i).showInfo();
+		}
 	}
 }
