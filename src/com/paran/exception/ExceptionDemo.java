0c1be4661c05e4403bf3242ff6b800a6855aee7d
taroakrkr <taroakrkr@gmail.com>
2017-07-10 10:33:44 +0900

    예외

--- src/com/paran/exception/ExceptionDemo.java
+++ src/com/paran/exception/ExceptionDemo.java
@@ -1,26 +1,31 @@
 package com.paran.exception;
 
 public class ExceptionDemo {
+	static void mm(){
+		methodA();
+	}
 	static void methodA(){
 		try{
 			methodB();
 		}catch(Exception e){
 			//to to something
+			//e.printStackTrace();
 		}
+		System.out.println("aa");
 	}
 	static void methodB(){
 		System.out.println("before calling methodC");
-		methodC();
+		methodC();// exception handler가 없음, A로 전파
 		System.out.println("after calling methodC");
 	}
 	static void methodC(){
 		System.out.println("before exception occured");
 		int i = 0;
-		i = i/0;
+		i = i/0; // 예외 발생 B로 전파
 		System.out.println("After exception occured in methodC");
 	}
 	public static void main(String[] args){
-		methodA();
+		mm();
 		System.out.println("main 정상 종료");
 	}
 }
