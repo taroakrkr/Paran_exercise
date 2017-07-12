443db0f84c0ea704bed62596d160a7dda80efae6
taroakrkr <taroakrkr@gmail.com>
2017-07-12 17:08:17 +0900

    Anonymous

---
+++ src/com/paran/nested/AnonymousDemo.java
@@ -0,0 +1,20 @@
+package com.paran.nested;
+
+abstract class AnonymousInner {
+	public abstract void myMethod();
+}
+
+interface Message {
+String greet();
+}
+
+public class AnonymousDemo {
+	public static void main(String args[]) {
+		AnonymousInner inner = new AnonymousInner() {
+			public void myMethod() {	
+				System.out.println("This is an example of anonymous inner class");		
+			}	
+		};
+		inner.myMethod();
+	}
+}
